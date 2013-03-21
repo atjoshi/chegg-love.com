ccaf551452ae9348b5fc0057ee1ea2b8124546f2
Ashit Johi <atjoshi@gmail.com>
2013-03-20 22:34:50 -0700

    adding empty classes for login and charity

---
+++ Business/CharityManager.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Description of CharityManager
+ *
+ * @author atjoshi
+ * 
+ */
+namespace Lovecom\Business;
+
+class CharityManager extends Lovecom\Business\BaseManager 
+{
+    public function getCharitiesByUser( $userId )
+    {
+        return array();
+    }
+    
+    public function saveUserCharities( $userId, $charityId )
+    {
+        
+    }
+}
